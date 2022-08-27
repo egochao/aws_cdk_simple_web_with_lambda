b83c426d3beddd85d89445406193a46d8677dab8
egochao <johnoldmanworkspace@gmail.com>
2022-08-27 10:45:37 +0000

    update frontend connection

--- services/functions/lambda.ts
+++ services/functions/lambda.ts
@@ -17,6 +17,21 @@ export async function handler() {
   // column called "tally"
   let count = results.Item ? results.Item.tally : 0;
 
+
+  const putParams = {
+    TableName: process.env.tableName,
+    Key: {
+      counter: "clicks",
+    },
+    // Update the "tally" column
+    UpdateExpression: "SET tally = :count",
+    ExpressionAttributeValues: {
+      // Increase the count
+      ":count": ++count,
+    },
+  };
+  await dynamoDb.update(putParams).promise();
+  
   return {
     statusCode: 200,
     body: count,
