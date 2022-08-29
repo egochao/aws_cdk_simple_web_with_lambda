03bca21c5b716028f538ee12dc6c0e0de6a45330
egochao <johnoldmanworkspace@gmail.com>
2022-08-29 02:19:14 +0000

    update note

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,13 +6,4 @@ export default defineConfig({
   test: {
     testTimeout: 30000,
   },
-  logLevel: "info",
-  esbuild: {
-    sourcemap: "both",
-  },
-  resolve: {
-    alias: {
-      "@gatsby-app/core": "./services/core",
-    },
-  },
 });
