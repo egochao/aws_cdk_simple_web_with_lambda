a60e2610081573d5c42b45fc6ed62d023d9f3310
egochao <johnoldmanworkspace@gmail.com>
2022-09-05 05:09:52 +0000

    add identity pool

--- lib/cognito_stack.ts
+++ lib/cognito_stack.ts
@@ -1,10 +1,12 @@
 import * as cognito from 'aws-cdk-lib/aws-cognito';
 import * as cdk from 'aws-cdk-lib';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 
 export class CognitoStack extends cdk.Stack {
     public readonly userPoolId: string;
     public readonly userPoolClientId: string;
+    public readonly IdentityPoolId: string | undefined;
     constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
@@ -29,14 +31,101 @@ export class CognitoStack extends cdk.Stack {
         });
 
 
+        const identityPool = new cognito.CfnIdentityPool(this, 'IdentityPool', {
+            identityPoolName: 'static-web-app-identity-pool',
+            allowUnauthenticatedIdentities: true,
+            cognitoIdentityProviders: [
+                {
+                    clientId: userPoolClient.userPoolClientId,
+                    providerName: userPool.userPoolProviderName,
+                },
+            ],
+        });
+      
+        
+        const isAnonymousCognitoGroupRole = new iam.Role(
+            this,
+            'anonymous-group-role',
+            {
+                description: 'Default role for anonymous users',
+                assumedBy: new iam.FederatedPrincipal(
+                'cognito-identity.amazonaws.com',
+                {
+                    StringEquals: {
+                    'cognito-identity.amazonaws.com:aud': identityPool.ref,
+                    },
+                    'ForAnyValue:StringLike': {
+                    'cognito-identity.amazonaws.com:amr': 'unauthenticated',
+                    },
+                },
+                'sts:AssumeRoleWithWebIdentity',
+                ),
+                managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName(
+                    'service-role/AWSLambdaBasicExecutionRole',
+                ),
+                ],
+            },
+            );
+
+        const isUserCognitoGroupRole = new iam.Role(this, 'users-group-role', {
+            description: 'Default role for authenticated users',
+            assumedBy: new iam.FederatedPrincipal(
+                'cognito-identity.amazonaws.com',
+                {
+                StringEquals: {
+                    'cognito-identity.amazonaws.com:aud': identityPool.ref,
+                },
+                'ForAnyValue:StringLike': {
+                    'cognito-identity.amazonaws.com:amr': 'authenticated',
+                },
+                },
+                'sts:AssumeRoleWithWebIdentity',
+            ),
+            managedPolicies: [
+                iam.ManagedPolicy.fromAwsManagedPolicyName(
+                'service-role/AWSLambdaBasicExecutionRole',
+                ),
+            ],
+            });
+
+        
+        new cognito.CfnIdentityPoolRoleAttachment(
+                this,
+                'identity-pool-role-attachment',
+                {
+                  identityPoolId: identityPool.ref,
+                  roles: {
+                    authenticated: isUserCognitoGroupRole.roleArn,
+                    unauthenticated: isAnonymousCognitoGroupRole.roleArn,
+                  },
+                  roleMappings: {
+                    mapping: {
+                      type: 'Token',
+                      ambiguousRoleResolution: 'AuthenticatedRole',
+                      identityProvider: `cognito-idp.${
+                        cdk.Stack.of(this).region
+                      }.amazonaws.com/${userPool.userPoolId}:${
+                        userPoolClient.userPoolClientId
+                      }`,
+                    },
+                  },
+                },
+              );
+              
+        
         this.userPoolId = userPool.userPoolId;
         this.userPoolClientId = userPoolClient.userPoolClientId;
-        
+        this.IdentityPoolId = identityPool.attrName || "None";
         new cdk.CfnOutput(this, 'UserPoolId', {
             value: this.userPoolId,
         });
         new cdk.CfnOutput(this, 'UserPoolClientId', {
             value: this.userPoolClientId,
         });
+        new cdk.CfnOutput(this, 'IdentityPoolId', {
+            value: this.IdentityPoolId,
+        });
+        
     }
 }
